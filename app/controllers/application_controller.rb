fdbc2139a1a69ace52ea70351b16185a38dd2830
rafaelgpimenta <rafaelgpimenta@gmail.com>
2020-07-13 13:19:09 -0300

    jwt config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+  private
+
+  def current_customer
+    @current_customer ||= Customer.find(payload['customer_id'])
+  end
+
+  def not_authorized
+    render json: { error: "Not authorized" }, status: :unauthorized
+  end
 end
---
+++ config/initializers/jwt_session.rb
@@ -0,0 +1,2 @@
+JWTSessions.algorithm = "HS256"
+JWTSessions.encryption_key = ENV['JWT_SECRET']
