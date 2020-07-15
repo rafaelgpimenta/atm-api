5d42ac30326dcbc91a221f0fafec3ebba8a05ba9
rafaelgpimenta <rafaelgpimenta@gmail.com>
2020-07-14 22:45:29 -0300

    config travis

--- config/initializers/jwt_session.rb
+++ config/initializers/jwt_session.rb
@@ -1,2 +1,7 @@
 JWTSessions.algorithm = "HS256"
-JWTSessions.encryption_key = ENV['JWT_SECRET']
+if Rails.env.test?
+  JWTSessions.encryption_key = 'secret'
+  JWTSessions.token_store = :memory
+else
+  JWTSessions.encryption_key = ENV['JWT_SECRET']
+end
