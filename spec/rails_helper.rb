28fabc8250e4aa8c09cd6cc753fcae7bb763be88
rafaelgpimenta <rafaelgpimenta@gmail.com>
2020-07-14 21:16:21 -0300

    testing auth requests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,6 +64,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include ActiveSupport::Testing::TimeHelpers
   config.include AuthHelpers
   config.include FactoryBot::Syntax::Methods
   JWTSessions.token_store = :memory
